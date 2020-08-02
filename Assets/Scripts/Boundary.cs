550441f6cf3fa1ac946c746629132e80f425d058
Saikat <saikatmandal8@gmail.com>
2020-08-03 04:07:12 +0600

    Ship moving

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    [System.Serializable]
+    struct Boundary
+    {
+        public float x_min, x_max, z_min, z_max;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    Boundary boundary;
+
+    [SerializeField]
+    float tilt;
+
+    private void FixedUpdate()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+
+        Rigidbody current_obj = gameObject.GetComponent<Rigidbody>();
+        current_obj.velocity = new Vector3(move_horizontal, 0.0f, move_vertical) * speed;
+        current_obj.position = new Vector3(
+            Mathf.Clamp(current_obj.position.x, boundary.x_min, boundary.x_max),
+            0.0f,
+            Mathf.Clamp(current_obj.position.z, boundary.z_min, boundary.z_max)
+            );
+        current_obj.rotation = Quaternion.Euler(0, 0, current_obj.velocity.x * -tilt);
+    }
+}
